6c1c55a28976c7b7ee0f7b974073e50979e3d19d
Rokeya-Islam <rokeyaislam90@gmail.com>
2022-02-17 13:12:30 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+// scroll reveal section
+window.sr = ScrollReveal();
+sr.reveal('#logo', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'top'
+});
+sr.reveal('.main-header', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('.slogan', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.button', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+sr.reveal('.pricing-slogan', {
+    duration: 2000,
+});
+sr.reveal('.card', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+
+// about us section animation
+sr.reveal('#about_us_1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('#about_us_2', {
+    duration: 2000,
+    // origin: 'left',
+    distance: '300px'
+});
+sr.reveal('#about_us_3', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+
+// service section animation
+sr.reveal('#service-image1', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('#service-image2', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('#service-image3', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.service-details1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.service-details3', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+
+// scroll reveal section ends
